b1e4390644650c91f8a1f4ae418963a56c1c71a1
hvardhan1024 <hvardhan1024@gmail.com>
2023-09-05 17:31:04 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <div> <Navbar/> </div>
+        <div> BODY </div>
+        <div> <Footer/> </div>
+
+    </div>
+  )
+}
