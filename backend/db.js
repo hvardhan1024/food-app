1286a83d30515970b39b755606f6bd2b32fb07f3
hvardhan1024 <hvardhan1024@gmail.com>
2023-09-20 07:38:24 +0530

    db connection and basic read and write operations performed

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,37 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const dbName = process.env.DBName;
 const dbPass = process.env.MONGO_PASS;
-const mongoURI = `mongodb+srv://${dbName}:${dbPass}@foodappmern.tcae3gf.mongodb.net/?retryWrites=true&w=majority`;
+const mongoURI = `mongodb+srv://${dbName}:${dbPass}@foodappmern.tcae3gf.mongodb.net/data?retryWrites=true&w=majority`;
+
 
 const connectMongoDB = async () => {
-  await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
-    console.log(" Connected to db ");
-    if (err) {
-      console.log(err);
-    }
-  });
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true });
+    console.log("Connected to MongoDB");
+
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray();
+    // console.log(data);
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 };
 
+
+
+// Reading data
+// const connectMongoDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI, { useNewUrlParser: true });
+//     console.log("Connected to MongoDB");
+
+//     const fetched_data = await mongoose.connection.db.collection("food_items");
+//     const data = await fetched_data.find({}).toArray();
+//     console.log(data);
+//   } catch (err) {
+//     console.error("MongoDB connection error:", err);
+//   }
+// };
+
 module.exports = connectMongoDB;
+ 
